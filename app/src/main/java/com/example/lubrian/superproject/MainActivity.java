9f436922c5aa46f2d46793a49fa7018436e6c5a5
brian_lu <brianb8888@gmail.com>
2017-11-02 17:16:21 +0800

    fix #2

--- app/src/main/java/com/example/lubrian/superproject/MainActivity.java
+++ app/src/main/java/com/example/lubrian/superproject/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.lubrian.superproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.lubrian.superproject.protocol.IdolData;
+import com.example.lubrian.superproject.protocol.IdolService;
+import com.example.lubrian.superproject.thriftManager.ThriftUtils;
+
+import org.apache.thrift.async.AsyncMethodCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private IdolService.AsyncIface asyncIface = ThriftUtils.getAsyncClient(StaticValue.SERVER_URL);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try{
+            asyncIface.get("BIGBANG", new AsyncMethodCallback<IdolData>() {
+                @Override
+                public void onComplete(IdolData response) {
+                    if(response != null){
+                        Log.e("debug","get idol:" + response.name);
+                    }
+                }
+
+                @Override
+                public void onError(Exception exception) {
+                    exception.printStackTrace();
+                    Log.e("debug","onError");
+                }
+            });
+        }catch (Exception e){
+            Log.e("debug","Error");
+        }
     }
 }
---
+++ app/src/main/java/com/example/lubrian/superproject/StaticValue.java
@@ -0,0 +1,11 @@
+package com.example.lubrian.superproject;
+
+/**
+ * Created by lubrian on 2017/11/2.
+ */
+
+public class StaticValue {
+
+    public static final String SERVER_URL = "http://192.168.0.115:8081/user_svc";
+
+}
